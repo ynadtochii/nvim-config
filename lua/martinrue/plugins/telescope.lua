b423911ee07be42f093e1e9dcf2e5559b1d7ceb8
evaries <nadtochii.e@gmail.com>
2024-07-26 17:32:18 +0800

    add grep with args

--- lua/martinrue/plugins/telescope.lua
+++ lua/martinrue/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-telescope/telescope-live-grep-args.nvim",
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
@@ -23,10 +24,21 @@ return {
 		})
 
 		telescope.load_extension("fzf")
+		telescope.load_extension("live_grep_args")
+
+		-- "foo bar" baz	search for „foo bar“ in dir „baz“
+		-- --no-ignore "foo bar	search for „foo bar“ ignoring ignores
+		-- "foo" --iglob **/test/**	search for „foo“ in any „test“ path
 
 		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
 		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
 		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
 		vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Fuzzy find files in cwd" })
+		vim.keymap.set(
+			"n",
+			"<leader>fg",
+			":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+			{ desc = "Fuzzy find with args" }
+		)
 	end,
 }
